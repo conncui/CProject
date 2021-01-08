03ac38874cc6dab8e4e3de5b733bfc764b6663bc
cuiguoqiang.kobe <cuiguoqiang.kobe@bytedance.com>
2021-01-08 17:13:02 +0800

    C++ Hello World

---
+++ hello_world.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+// main() 是程序开始执行的地方
+ 
+int main()
+{
+   cout << "Hello World"; // 输出 Hello World
+   return 0;
+}
+
+/**
+
+【1】编译&运行
+$ g++ hello.cpp
+$ ./a.out
+Hello World
+
+*/
